febab8c2588e76ccf6f58cf4b40cc217c34b9e58
Szymon Urbaś <szymon.urbas@aol.com>
2013-05-31 23:10:18 +0200

    Add a few more commands.

--- script.js
+++ script.js
@@ -16,36 +16,117 @@ $(document).ready(function(){
   // handle the Enter keydown
   cmd.keypress(function(e){
     if (e.which == 13){
-      handle_input(cmd.val());
-      // clear out the input
-      cmd.val("");
+      /* do anything only if there is something in the input */
+      if (cmd.val() != ""){
+        handle_input(cmd.val());
+        // clear out the input
+        cmd.val("");
+      }
     }
   });
 });
 
 function handle_input(input)
 {
-  var dollar = $("#dollar");
-  var cmd    = $("#cmd");
+  var args = input.split(" ");
+  var cmd  = args[0];
+
+  switch (cmd){
+    case "ls":
+      out(".<br>..<br>hello");
+      break;
+    case "copyright":
+      out("Copyleft (c) 2013 by Szymon Urbaś &lt;<a href='mailto:szymon.urbas@aol.com'>szymon.urbas@aol.com</a>&gt;");
+      break;
+    case "echo":
+      out(args.slice(1).join(" "));
+      break;
+    case "clear":
+      out();
+      break;
+    case "uname":
+      switch (args[1]){
+        case "-a":
+          out("Linux little-big-horn 3.5.7-gentoo #7 SMP Wed Apr 3 21:48:46 UTC 2013 x86_64 Intel(R) Core(TM)2 Duo CPU T6570 @ 2.10GHz GenuineIntel GNU/Linux");
+          break;
+        case "-n":
+          out("little-big-horn");
+          break;
+        case "-r":
+          out("3.5.7-gentoo");
+          break;
+        case "-s":
+          /* fall through */
+        case undefined:
+          out("Linux");
+          break;
+        case "-v":
+          out("#7 SMP Wed Apr 3 21:48:46 UTC 2013");
+          break;
+        case "-m":
+          out("x86_64");
+          break;
+        case "-p":
+          out("Intel(R) Core(TM)2 Duo CPU T6570 @ 2.10GHz");
+          break;
+        case "-i":
+          out("GenuineIntel");
+          break;
+        case "-o":
+          out("GNU/Linux");
+          break;
+        case "--help":
+          /* there is GOT to be better way to do this */
+          out("Usage: uname [OPTION]...<br>Print certain system information.  With no OPTION, same as -s.<br><br><br>  -a, --all                print all information, in the following order,<br>                             except omit -p and -i if unknown:<br>  -s, --kernel-name        print the kernel name<br>  -n, --nodename           print the network node hostname<br>  -r, --kernel-release     print the kernel release<br>  -v, --kernel-version     print the kernel version<br>  -m, --machine            print the machine hardware name<br>  -p, --processor          print the processor type or \"unknown\"<br>  -i, --hardware-platform  print the hardware platform or \"unknown\"<br>  -o, --operating-system   print the operating system<br>      --help     display this help and exit<br>      --version  output version information and exit<br><br>Report uname bugs to bug-coreutils@gnu.org<br>GNU coreutils home page: <http://www.gnu.org/software/coreutils/><br>General help using GNU software: <http://www.gnu.org/gethelp/><br>For complete documentation, run: info coreutils 'uname invocation'");
+          break;
+        default:
+          out("uname: invalid option -- '" + args[1] + "'<br>Try 'uname --help' for more information.");
+          break;
+      }
+      break;
+    case "cat":
+      if (args[1] === "/dev/urandom"){
+        out("sending an e-mail to Chuck Norris asking about the contents of /dev/urandom...");
+        setTimeout(function(){ out("OH NOEZ"); }, 3000);
+        setTimeout(function(){ out("stack overflow!"); }, 3000);
+        setTimeout(function(){ out("call the ambulance!"); }, 3000);
+        setTimeout(function(){ out("oh, no, wait.. here it is!"); }, 3000);
+        setTimeout(function(){ out("/* some crap */"); }, 3000);
+      } else {
+        out();
+      }
+      break;
+    default:
+      out(cmd + ": command not found");
+      break;
+  }
+}
+
+function out(output)
+{
   var resbox = $("#resbox");
 
-  if (input == "ls"){
-    resbox.html(".<br>..");
-  }
-  else if (input == "clear"){
+  resbox.animate({
+    height: '0',
+    padding: '0'
+  }, {
+    duration: 50,
+    easing: 'easeOutBounce'
+  });
+
+  if (output == null){
     resbox.html("");
+    resbox.css("padding", 0);
+    resbox.css("height", 0);
   }
   else {
-    resbox.html("command not found");
+    resbox.html(output);
+    resbox.animate({
+      height: '100%',
+      padding: '20'
+    }, {
+      duration: 100,
+      easing: 'easeOutBounce'
+    });
   }
-
-  resbox.css("padding", 20);
-  resbox.animate({
-    height: '100%'
-  }, {
-    duration: 100,
-    specialEasing: {
-      height: 'easeOutBounce'
-    }
-  });
 }
